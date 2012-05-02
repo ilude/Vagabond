edf72c044656f419d0398271ccb72f1a1b73e39f
Mike Glenn <mike.glenn@rammount.com>
2012-05-02 10:14:07 -0400

    update group permissions

--- templates/ubuntu-12.04-server-amd64/latecommand.sh
+++ templates/ubuntu-12.04-server-amd64/latecommand.sh
@@ -1,6 +1,6 @@
 /bin/cp /etc/sudoers /etc/sudoers.orig
-/bin/sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=admin' /etc/sudoers
-/bin/sed -i -e 's/%admin ALL=(ALL) ALL/%admin ALL=NOPASSWD:ALL/g' /etc/sudoers
+/bin/sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=adm' /etc/sudoers
+/bin/sed -i -e 's/%admin ALL=(ALL) ALL/%adm ALL=NOPASSWD:ALL/g' /etc/sudoers
 
 /bin/cp /etc/ssh/sshd_conf /etc/ssh/sshd_conf.orig
 /bin/sed -i -e 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_conf
